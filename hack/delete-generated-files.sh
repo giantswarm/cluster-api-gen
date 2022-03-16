444828972c7c5ec4a63d8c9925026fcbfbe99dab
Nikola Prokopić <nikola@prokopic.rs>
2022-03-16 13:32:27 +0100

    Delete empty directories when deleting generated files

--- hack/delete-generated-files.sh
+++ hack/delete-generated-files.sh
@@ -10,5 +10,20 @@ cd ..
 for file in $(yq e ".generated[]" apigen.lock); do
     if [ -f "$file" ]; then
         rm "$file"
+
+        # delete directory if empty
+        parent_dir="$(dirname "$file")"
+        if [ -z "$(ls -A "$parent_dir")" ]; then
+           rm -r "$parent_dir"
+
+           # check all parent dirs all the way to project root
+           parent_dir="$(dirname "$parent_dir")"
+           while [[ $parent_dir != "." ]]; do
+               if [ -z "$(ls -A "$parent_dir")" ]; then
+                   rm -r "$parent_dir"
+               fi
+               parent_dir="$(dirname "$parent_dir")"
+           done
+        fi
     fi
 done
